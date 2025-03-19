4a33679472d59adc616a145d3f32a7969d313143
Nandhini299 <nandhinni14082005@gmail.com>
2025-03-19 23:40:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var img = document.querySelector("img");
+function on()
+{
+   // body.className ="";
+    img.classList.add("fan");
+}
+
+function off()
+{
+    //body.className="";
+    img.classList.remove("fan");
+}
